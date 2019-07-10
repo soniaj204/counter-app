0df0160ac6e41be08ed3363783e2e0ca66ff3bf1
Sonia Jain <baps@bapss-MacBook-Pro-2.local>
2019-07-10 16:07:30 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,73 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Counter from "./components/Counter";
+import { BrowserRouter as Router, NavLink, Redirect } from "react-router-dom";
+import Route from "react-router-dom/Route";
 
-function App() {
-  return (
-    <div className="App">
-      <Counter />
-    </div>
-  );
+const User = (params) => {
+  return <h1>Welcome User {params.username}</h1>;
+};
+
+class App extends Component {
+  state = {
+    loggedIn: false
+  };
+
+  logInHandle = () => {
+    this.setState({ loggedIn: true });
+  };
+
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <NavLink to="/" activeStyle={{ color: "green" }} exact>
+            Home
+          </NavLink>
+          <NavLink to="/about" activeStyle={{ color: "green" }} exact>
+            About
+          </NavLink>
+          <NavLink to="/user/sonia" activeStyle={{ color: "green" }} exact>
+            User Sonia
+          </NavLink>
+          <NavLink to="/user/jain" activeStyle={{ color: "green" }} exact>
+            User Jain
+          </NavLink>
+
+          <input
+            type="button"
+            value="log In"
+            onClick={this.logInHandle.bind(this)}
+          />
+
+          <Route
+            path="/"
+            exact
+            strict
+            render={() => {
+              return <h1>Welcome</h1>;
+            }}
+          />
+          <Route
+            path="/about"
+            exact
+            strict
+            render={() => {
+              return <h1>Welcome About</h1>;
+            }}
+          />
+
+          <Route
+            path="/user/:username"
+            exact
+            strict
+            render={({match}) => (this.state.loggedIn ? <User username={match.params.username} /> : <Redirect />)}
+          />
+          <Counter />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
